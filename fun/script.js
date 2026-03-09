8d9b365d53eb5ced0066ecdbe84109139613b394
JeronimoCabanelas <jeronimo.cabanela@gmail.com>
2026-03-09 10:37:27 -0500

    rp

--- fun/script.js
+++ fun/script.js
@@ -1,14 +1,21 @@
-const btnSim = document.getElementById("btnSim")
-const btnNao = document.getElementById("btnNao")
+const btnSim = document.getElementById("btnSim");
+const btnNao = document.getElementById("btnNao");
+
+btnSim.addEventListener("click", aceitar);
+btnNao.addEventListener("mouseover", desviar);
+btnNao.addEventListener("click", desviar);
 
 function aceitar(){
-    alert("Você aceitou namorar comigo :>")
-    location.href = "https://www.youtube.com/shorts/X0hUMlgLQEY?feature=share"
+    alert("Você aceitou namorar comigo :>");
+    location.href = "https://www.youtube.com/shorts/X0hUMlgLQEY?feature=share";
 }
 
 function desviar(){
-    btnNao.style.position = "absolute";
+    btnSim.style.position = "absolute";
+    btnNao.style.bottom = geraPosicao(10, 90);
+    btnNao.style.left = geraPosicao(10, 90);
 
+    console.log("Opa, desviei...");
 }
 
 
